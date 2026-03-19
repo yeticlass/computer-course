bea6d52caab83b2fdc30a86c3dee7c69160581a1
yeticlass <sangbu075@gmail.com>
2026-03-19 20:47:22 +0545

    text-update

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,15 @@ const  Main= () => {
   return (
     <>
     <h1 className="text-red-500">test!</h1>
+    <section>
+
+
+    <button className="
+    bg-slate-300 px-2 
+    hover:bg-amber-300
+    hover:cursor-pointer
+    rounded-md">OK</button>
+    </section>
     </>
     );
 }
